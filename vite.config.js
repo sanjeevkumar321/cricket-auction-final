a35d8cc7739222cce955a06a40254b91c628d980
sanjeev-diggibyte <sanjeev.kumarsahoo@diggibyte.com>
2025-07-02 08:05:57 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/cricket-auction",
+  base: "/cricket-auction-final",
 });
